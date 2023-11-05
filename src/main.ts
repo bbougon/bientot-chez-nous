8bf625c71a1af7776452fd171df6147711237e86
Bertrand Bougon <bertrandbougon@gmail.com>
2023-11-05 22:19:20 +0100

    Refacto: transforme en projet node

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import studio from "@theatre/studio";
+import {getProject, types} from "@theatre/core";
+import projectState from "./state.json";
+import './style.css';
+
+if(import.meta.env.DEV) {
+    studio.initialize()
+}
+console.log(projectState);
+const project = getProject('Bientôt chez nous', {state: projectState})
+const sheet = project.sheet('BD')
+const obj = sheet.object('Planche', {
+    position: {
+        x: 0,
+        y: 0,
+        z: 1,
+    },
+    opacity: types.number(1, { range: [0, 1] }), // or use a type constructor to customize
+})
+
+const articleHeadingElement = document.getElementById('planche')
+
+obj.onValuesChange((obj) => {
+    articleHeadingElement!.style.transform = `scale(${obj.position.z}) translateY(${obj.position.y}px) translateX(${obj.position.x}px)`
+    articleHeadingElement!.style.opacity = String(obj.opacity)
+})
+
+project.ready.then(() => {
+
+
+    sheet.sequence.attachAudio({ source: '/musique.mp3' }).then(() => {
+        console.log('Audio loaded!')
+        return sheet.sequence.play({ iterationCount: Infinity })
+    })
+
+})
